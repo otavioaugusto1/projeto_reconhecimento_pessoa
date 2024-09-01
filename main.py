4de75ea0a4a136cf994b05eb5f1192c519c692f9
Otávio Augusto Gonçalves Nogueira <56416850+otavioaugusto1@users.noreply.github.com>
2024-08-31 22:25:11 -0300

    Update main.py
    Alteraçoes de codigo

--- main.py
+++ main.py
@@ -17,9 +17,9 @@ rtsp_urls = [
     # Adicione mais endereços de câmeras conforme necessário
 ]
 
-# Duração do buffer para armazenar 60 segundos de vídeo
+# Buffer para armazenar 60 segundos de vídeo
 buffer_duration = 60
-frame_rate = 20  # Taxa de quadros aproximada (ajuste conforme a realidade das câmeras)
+frame_rate = 20  # Taxa de quadros aproximada (altere dependendo da qualidade  de suas cameras)
 
 # Função para salvar o vídeo quando uma pessoa é detectada
 def save_video(frames, camera_id):
@@ -43,7 +43,7 @@ def process_camera(rtsp_url, camera_id):
         print(f"Não foi possível acessar a câmera {camera_id}. Verifique a conexão.")
         return
 
-    # Buffer circular para armazenar os últimos 60 segundos de vídeo
+    # Buffer para armazenar os últimos 60 segundos de vídeo
     buffer = collections.deque(maxlen=frame_rate * buffer_duration)
     
     while True:
@@ -54,7 +54,6 @@ def process_camera(rtsp_url, camera_id):
 
         buffer.append(frame)  # Armazena o frame no buffer para possível salvamento futuro
 
-        # Pré-processamento do frame para detecção de objetos
         blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)), 0.007843, (300, 300), 127.5)
         net.setInput(blob)
         detections = net.forward()
@@ -64,17 +63,17 @@ def process_camera(rtsp_url, camera_id):
             confidence = detections[0, 0, i, 2]
             if confidence > 0.2:  # Apenas detecções confiáveis
                 idx = int(detections[0, 0, i, 1])
-                if idx == 15:  # O índice 15 no modelo Caffe corresponde à classe "person"
+                if idx == 15: 
                     box = detections[0, 0, i, 3:7] * np.array([frame.shape[1], frame.shape[0], frame.shape[1], frame.shape[0]])
                     (startX, startY, endX, endY) = box.astype("int")
                     cv2.rectangle(frame, (startX, startY), (endX, endY), (0, 255, 0), 2)
                     label = "{}: {:.2f}%".format(PERSON_CLASS, confidence * 100)
                     cv2.putText(frame, label, (startX, startY - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
 
-                    # Salva os últimos 60 segundos de vídeo ao detectar uma pessoa
+                    # Salva os últimos 60 segundos de vídeo quando possivelmente acha uma pessoa
                     save_video(list(buffer), camera_id)
 
-        # Exibe o frame com as detecções
+        # Exibe o trecho com as detecções
         cv2.imshow(f"Camera {camera_id}", frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
